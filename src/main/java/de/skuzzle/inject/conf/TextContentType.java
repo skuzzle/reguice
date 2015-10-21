b2218c66bc1ec2f68c74645398afdd1be074c7c2
Simon Taddiken <simon.taddiken@gmail.com>
2015-10-21 08:01:05 +0200

    Add comment

--- src/main/java/de/skuzzle/inject/conf/TextContentType.java
+++ src/main/java/de/skuzzle/inject/conf/TextContentType.java
@@ -1,6 +1,25 @@
 package de.skuzzle.inject.conf;
 
+import de.skuzzle.inject.conf.Resources.ChoseTargetType;
+
+/**
+ * This interfaces controls how the content of a {@link TextResource} is mapped
+ * to a concrete instance of some object.
+ *
+ * @author Simon Taddiken
+ */
 public interface TextContentType {
 
+    /**
+     * Creates an instance of type T from the given resource. The type that is
+     * passed here, is the one that the user passed to
+     * {@link ChoseTargetType#to(Class)} when binding a resource.
+     *
+     * <T> The result type.
+     *
+     * @param type The type to create an instance of.
+     * @param resource The resource to create an instance from.
+     * @return The created Object.
+     */
     <T> T createInstance(Class<T> type, TextResource resource);
 }
