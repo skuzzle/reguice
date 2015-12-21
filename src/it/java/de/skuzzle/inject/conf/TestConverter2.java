89bcc44db5b28e83882274b63754dc4145222282
Simon Taddiken <simon.taddiken@gmail.com>
2015-12-21 19:49:43 +0100

    Add duplicated converter

---
+++ src/it/java/de/skuzzle/inject/conf/TestConverter2.java
@@ -0,0 +1,15 @@
+package de.skuzzle.inject.conf;
+
+public class TestConverter2 implements Converter<SampleObject> {
+
+    @Override
+    public Class<SampleObject> forType() {
+        return SampleObject.class;
+    }
+
+    @Override
+    public SampleObject parseString(String s) throws ConverterException {
+        return new SampleObject(s.split(","));
+    }
+
+}
